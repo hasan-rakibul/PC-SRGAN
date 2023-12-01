e988ceeb67a6aa5a9f0cffd16f0c362e3812f2b8
Rakibul Hasan <rakibul.eeekuet@gmail.com>
2023-12-01 16:05:28 +0800

    FEM prepared

--- README.md
+++ README.md
@@ -18,6 +18,17 @@ To deactivate the current environment:
 deactivate
 ```
 
+## FeniCS
+pip install is so much pain; it needs to build from source because of the C++ dependencies. The failed attempt with pip at the end of this document.
+
+Better to install using conda:
+```bash
+conda create -n fenics python=3.9.4
+conda activate fenics
+conda install -c conda-forge fenics
+```
+And it worked like a charm!
+
 ## CSIRO Bracewell
 Python version is **3.9.4**
 
@@ -33,23 +44,23 @@ python -m pip install -r requirements-SRGAN.txt
 
 ## Local machine
 
-I first install python3.11 in Ubuntu 22.04.3 LTS  
+I first install python3.9 in Ubuntu 22.04.3 LTS  
 
 ```bash
-sudo apt install python3.11 
-sudo apt install python3.11-venv 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9 python3.9-venv python3.9-distutils
 ```
 
 Create and activate virtual environment.
 ```bash
-python3.11 -m venv ~/.venv/pinn
+python3.9 -m venv ~/.venv/pinn
 source ~/.venv/pinn/bin/activate 
 ```
 
 Install necessary files on the virtual environment 
 ```bash
 python -m pip install -r requirements-SRGAN.txt
- # python -m pip install -r requirements.txt 
+python -m pip install -r requirements.txt 
 ```
 
 # Others' code/data
@@ -89,8 +100,7 @@ Install python3.7 first and then tensorflow 1.15:
 ```bash
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.7
-sudo apt install python3.7-distutils
+sudo apt install python3.7 python3.7-distutils
 python3.7 -m pip install -r requirements-Bao22.txt
 python3.7 -m pip install -U Pillow
 ```
@@ -119,3 +129,36 @@ python -m pip install -r requirements-Bao22.txt
 - [https://confluence.csiro.au/display/SC/Requesting+resources+in+Slurm](https://confluence.csiro.au/display/SC/Requesting+resources+in+Slurm)
 - [https://confluence.csiro.au/display/SC/Running+jobs+in+an+interactive+batch+shell](https://confluence.csiro.au/display/SC/Running+jobs+in+an+interactive+batch+shell)
 - [https://confluence.csiro.au/display/GEES/HPC+Cheat+Sheet](https://confluence.csiro.au/display/GEES/HPC+Cheat+Sheet)
+
+
+# Failed attempt to install FeniCS using pip
+Taken and adapted from [https://fenics.readthedocs.io/en/latest/installation.html](https://fenics.readthedocs.io/en/latest/installation.html)
+
+```bash
+PYBIND11_VERSION=2.2.3
+wget -nc --quiet https://github.com/pybind/pybind11/archive/v${PYBIND11_VERSION}.tar.gz
+tar -xf v${PYBIND11_VERSION}.tar.gz && cd pybind11-${PYBIND11_VERSION}
+mkdir build && cd build && cmake -DPYBIND11_TEST=off .. && sudo make install
+
+pip3 install fenics-ffc --upgrade
+```
+Install [boost](https://www.boost.org/) (because the cmake in the below dolfin installation failed, saying boost not found): [here's a tutorial](https://linux.how2shout.com/how-to-install-boost-c-on-ubuntu-20-04-or-22-04/)
+
+Could NOT find Eigen3, so installed using `sudo apt install libeigen3-dev`
+
+There were two files where I needed to add `#include <algorithm>` because it didn't recognise `std::count` and `std::min_element`. YOu will know which files when you get the error.
+
+`endian.hpp` location is changed in recent boost version. In two files, `#include <boost/detail/endian.hpp>` needed to be changed to `#include <boost/predef/other/endian.h>`
+
+
+```bash
+FENICS_VERSION=$(python -c"import ffc; print(ffc.__version__)")
+git clone --branch=$FENICS_VERSION https://bitbucket.org/fenics-project/dolfin
+git clone --branch=$FENICS_VERSION https://bitbucket.org/fenics-project/mshr
+mkdir dolfin/build && cd dolfin/build && cmake .. && sudo make install && cd ../..
+mkdir mshr/build   && cd mshr/build   && cmake .. && make install && cd ../..
+cd dolfin/python && pip3 install . && cd ../..
+cd mshr/python   && pip3 install . && cd ../..
+```
+
+Lastly there's the error I coudn't solve: mshr bitbucket link was shown invalid and 'ERROR: Failed building wheel for fenics-dolfin' with `make install` of dolfin.
---
+++ src/FEM.py
@@ -0,0 +1,191 @@
+# This source file is primarily taken from Pouria Behnoudfar and later modified by Rakibul Hasan
+
+from fenics import *
+from dolfin import *
+import numpy as np
+import time
+
+def generate_data(eps, K, b1, b2, elem_per_dim):
+    # BCs; L: left, R: right, U: up, B: bottom
+    u_L = 1
+    u_R = 0
+    u_U = 0
+    u_B = 0
+
+    # Initial condition
+
+    # u_0 = Expression('sin(x[0])*sin(x[1])', degree=2)
+    u_0 = Expression('exp(-10*x[0])', degree=2)
+    # u_0 = Constant(0)
+
+    #######-------------------------------------------------------
+
+    n_ele = elem_per_dim    # no. of elements per dimension
+    num_steps = 100    # number of time steps
+    dt = T / num_steps # time step size
+    # Define expressions used in variational forms
+    dt = Constant(dt)
+    K = Constant(K)
+    eps = Constant(eps)
+    beta_vec = as_vector([Constant(b1),Constant(b2)]) 
+
+    # ---- G-alpha parameters
+
+    ro = 0.9
+
+    am = 1/2*(3-ro)/(1+ro)
+    af = 1/(1+ro)
+    gamma = 0.5-af+am
+    #### 
+
+    def initial_v(u_0,v_n,bcs_0):
+        F_0 = v_n*s*dx + dot(beta_vec,grad(u_0))*s*dx \
+        + eps*dot(grad(u_0), grad(s))*dx \
+        - K*u_0*u_0*(1-u_0)*s*dx                # this term is the reaction nonlinear function
+        - f_3*s*dx                        # Sourcing term
+
+        solve(F_0 == 0, v_n, bcs_0)
+        v_n.assign(v_n)
+
+        return 
+
+    def update(u_n,v_n,u_af):
+
+        u_nn.vector()[:]=(u_af.vector()[:]-u_n.vector()[:])/af+u_n.vector()[:]
+        v_nn.vector()[:]= (u_nn.vector()[:]-u_n.vector()[:]-dt*v_n.vector()[:])/dt/gamma+v_n.vector()[:]
+
+        u_n.assign(u_nn)
+        v_n.assign(v_nn)
+        return
+
+
+    x1 = 0
+    x2 = 1
+    y1 = 0
+    y2 = 1
+    # rect = Rectangle(Point(x1, y1), Point(x2, y2))
+    # mesh  = generate_mesh(rect1, n_ele)
+
+    mesh = UnitSquareMesh(n_ele,n_ele)
+
+
+    # Define function space
+    P_order = 2     # Polynomial order
+    Fun_sp = FunctionSpace(mesh, "CG", P_order)
+
+    # Define test functions
+    s = TestFunction(Fun_sp)
+
+    # Define functions for velocity and concentration
+
+    u = Function(Fun_sp)
+    u_n = Function(Fun_sp)
+    u_nn = Function(Fun_sp)
+    v_n = Function(Fun_sp)
+    v_nn = Function(Fun_sp)
+
+    f_3 = Constant(0)
+
+
+    F = am/(dt*gamma*af)*u*s*dx + dot(beta_vec,grad(u))*s*dx \
+    + eps*dot(grad(u), grad(s))*dx \
+    - K*u*u*(1-u)*s*dx - f_3*s*dx  \
+    - am/(dt*gamma*af)*u_n*s*dx - (1-am/gamma) *v_n*s*dx
+
+
+    # Boundary data 
+
+    tol = 1e-14
+
+
+    def boundary_right(x, on_boundary):
+        return on_boundary and near(x[0], x2, tol)
+
+    def boundary_left(x, on_boundary):
+        return on_boundary and near(x[0], x1, tol)
+
+    def boundary_bottom(x, on_boundary):
+        return on_boundary and near(x[1], y1, tol)
+
+    def boundary_up(x, on_boundary):
+        return on_boundary and near(x[1], y2, tol)
+
+
+    bc_L = DirichletBC(Fun_sp, u_L, boundary_left)
+    bc_R = DirichletBC(Fun_sp, u_R, boundary_right)
+    bc_B = DirichletBC(Fun_sp, u_B, boundary_bottom)
+    bc_U = DirichletBC(Fun_sp, u_U, boundary_up)
+
+    bcs = [bc_L, bc_R, bc_U, bc_B]
+
+    file_prefix = 'data/reaction_diffusion_advection/mesh_' + str(elem_per_dim) + '/eps_' + str(eps) + '_K_' + str(K) + \
+    # Create VTK files for visualization output
+    '_b1_' + str(b1) + '_b2_' + str(b2)
+    
+    vtkfile_u = File(file_prefix + '.pvd')
+
+    # Initial solution ------
+
+    u_n = interpolate(u_0, Fun_sp)
+    initial_v(u_n,v_n,bcs)
+
+    # Time-stepping
+    t = 0
+
+    for counter in range(num_steps):
+
+        vtkfile_u << (u_n, t)
+
+        t += dt
+        # Solve variational problem for time step
+        solve(F == 0, u, bcs)    # here we solve for u^{n+\alpha_f}
+
+        # Update previous solution
+        update(u_n,v_n,u)
+
+        # v_n.assign(solv_n)
+            # print('u max: ', u.vector().array().max())
+            
+        print('u max: ', np.max(np.array(u.vector()[:])))
+        # u_n.assign(u)
+
+        coords = Fun_sp.tabulate_dof_coordinates()
+        vec = u_n.vector().get_local()
+        outfile = open(file_prefix + ".txt", "w")
+        for coord, val in zip(coords, vec):
+            print(coord[0], coord[1], val, file=outfile)
+        outfile.close()
+
+def main():
+    start_time = time.time()
+
+    ###### Problem's Parameters
+    # 
+    T = 0.5 # final time
+
+    eps_range = np.linspace(1e-1, 1, 10) # diffusion coefficient
+    K_range = np.linspace(0, 1, 5) # reaction rate
+    
+    r_range = np.linspace(0, 1, 5)
+    theta_range = np.linspace(-np.pi/4, np.pi/4, 5)
+    
+    count = 0
+    total = len(eps_range) * len(K_range) * len(r_range) * len(theta_range)
+    for eps in eps_range:
+        for K in K_range:
+            for r in r_range:
+                for theta in theta_range:
+                    b1 = r * np.cos(theta) # Velocity component in x direction
+                    b2 = r * np.sin(theta) # Velocity component in y direction
+                    generate_data(eps, K, b1, b2, 8) # generating data for 8x8 mesh
+                    generate_data(eps, K, b1, b2, 64) # generating data for 64x64 mesh
+                    
+                    # status update
+                    count += 1
+                    print(f"Progress: {count}/{total}")
+                    print(f"Time elapsed: {time.time() - start_time}")
+                    print("--------------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
