0d7ebbc1b208a113e7172becd610aa8575668a65
Rakibul <rakibul.eeekuet@gmail.com>
2025-04-28 22:40:17 +0800

    additional configs in a separate dir; rrdb or esrgan - physics or no-physics in job

--- job_additional_baseline_setonix.sh
+++ job_additional_baseline_setonix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-#SBATCH --job-name=ESRGAN
+#SBATCH --job-name=RRDB-ESRGAN
 #SBATCH --output=bash_logs/%j_%x.log
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
@@ -10,20 +10,31 @@
 
 module load pytorch/2.2.0-rocm5.7.3
 
-################## Train RRDB - i.e., Pretraining for ESRGAN ##################
+################## Pretraining RRDB (or using it as SR No Physics) for ESRGAN ##################
+# singularity exec $SINGULARITY_CONTAINER bash -c "\
+# source .venv/bin/activate && \
+# python additional_baselines/train_rrdb.py \
+# --config_path=additional_baselines/configs/train_rrdb_no-physics.yaml"
+
+################## Train RRDB (Physics) as SR Model ##################
+# singularity exec $SINGULARITY_CONTAINER bash -c "\
+# source .venv/bin/activate && \
+# python additional_baselines/train_rrdb.py \
+# --config_path=additional_baselines/configs/train_rrdb_physics.yaml"
+
+# ################## Train ESRGAN (No Physics) ##################
+# singularity exec $SINGULARITY_CONTAINER bash -c "\
+# source .venv/bin/activate && \
+# python additional_baselines/train_esrgan.py \
+# --config_path=additional_baselines/configs/train_esrgan_no-physics.yaml"
+
+# ################## Train ESRGAN (Physics) ##################
 singularity exec $SINGULARITY_CONTAINER bash -c "\
 source .venv/bin/activate && \
-python additional_baselines/train_rrdb.py"
+python additional_baselines/train_esrgan.py \
+--config_path=additional_baselines/configs/train_esrgan_physics.yaml"
 
-
-#@@@@@@@@@@@@@@@ Test RRDB - make sure to set the correct (pre-determined, if doing train-test at one go) 
-# path to the test data
+################## Test - make sure to set the correct (pre-determined, if doing train-test at one go) 
 # singularity exec $SINGULARITY_CONTAINER bash -c "\
 # source .venv/bin/activate && \
 # python additional_baselines/test_esrgan.py"
-
-
-################## Train ESRGAN ##################
-# singularity exec $SINGULARITY_CONTAINER bash -c "\
-# source .venv/bin/activate && \
-# python additional_baselines/train_esrgan.py"
